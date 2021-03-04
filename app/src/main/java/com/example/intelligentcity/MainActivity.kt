936bb29b08a1d5f4be833106de4e1a50f169bc57
Tiago Pereira <tjosepereira@ipvc.pt>
2021-03-04 00:58:38 +0000

    painel inicial

--- app/src/main/java/com/example/intelligentcity/MainActivity.kt
+++ app/src/main/java/com/example/intelligentcity/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.intelligentcity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun login(view: View) {}
+    fun registar_Utilizador(view: View) {}
+    fun consultar(view: View) {}
 }
