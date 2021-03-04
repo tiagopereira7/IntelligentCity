b4e6374bceb33489ff07b55bde10b5f57a5a9c69
Tiago Pereira <tjosepereira@ipvc.pt>
2021-03-04 01:30:03 +0000

    Layout ecra registo

---
+++ app/src/main/java/com/example/intelligentcity/RegistarActivity.kt
@@ -0,0 +1,17 @@
+package com.example.intelligentcity
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistarActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registar)
+    }
+
+fun registar(view: View) {}
+
+}
+
+
