9d5709ef0d39c96c29b7e4afbd0ebc15a4d0e698
Tiago Pereira <tjosepereira@ipvc.pt>
2021-03-16 02:45:37 +0000

    noteAdapter

---
+++ app/src/main/java/com/example/intelligentcity/adapters/NoteAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.intelligentcity.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.intelligentcity.R
+import com.example.intelligentcity.entities.Note
+
+class NoteAdapter internal constructor(
+    context: Context
+) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var notes = emptyList<Note>()
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val noteItemView: TextView = itemView.findViewById(R.id.layout_linha_card_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val itemView = inflater.inflate(R.layout.layout_linha, parent, false)
+        return NoteViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val current = notes[position]
+        holder.noteItemView.text = current.title + "-" + current.text
+    }
+
+    internal fun setNotes(notes: List<Note>) {
+        this.notes = notes
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = notes.size
+}
